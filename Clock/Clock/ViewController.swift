7be74e0124af174571b9f17f1d09a2a4aa26e67c
John DongHyun Kim <zapping0320@gmail.com>
2019-09-27 07:12:04 +0900

    show currnet time

--- Clock/Clock/ViewController.swift
+++ Clock/Clock/ViewController.swift
@@ -19,28 +19,66 @@ class ViewController: UIViewController {
     private var currentColorRule:[UIColor] = []
     private var isSetRuleDefault = true
     
+    private var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //ser defaultValue
-        currentColorRule.append(ColorHelper.textColorBlue)
         currentColorRule.append(ColorHelper.textColorRed)
+        currentColorRule.append(ColorHelper.textColorBlue)
         
+        timeHourLabel.text = "00"
+        timeLeftSeperatorLabel.text = ":"
+        timeMinuteLabel.text = "00"
+        timeRightSeperatorLabel.text = ":"
+        timeSecondLabel.text = "00"
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateUI), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateUI() {
+        let date = Date()
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.hour, .minute, .second], from: date)
+        let colorIndex = components.second! % 2
+        let currentTextColor = currentColorRule[colorIndex]
         
+        let dateFormat = DateFormatter()
+        dateFormat.dateFormat = "HH"
+        timeHourLabel.text = dateFormat.string(from: date)
+        timeHourLabel.textColor = currentTextColor
+        
+        timeLeftSeperatorLabel.textColor = currentTextColor
+        
+        dateFormat.dateFormat = "mm"
+        timeMinuteLabel.text = dateFormat.string(from: date)
+        timeMinuteLabel.textColor = currentTextColor
+        
+        timeRightSeperatorLabel.textColor = currentTextColor
+        
+        dateFormat.dateFormat = "ss"
+        timeSecondLabel.text = dateFormat.string(from: date)
+        timeSecondLabel.textColor = currentTextColor
         
     }
+    
 
     @IBAction func changeTextcolorRule(_ sender: Any) {
+        var updatedColorRule : [UIColor] = []
         if(self.isSetRuleDefault)
         {
-            currentColorRule.append(ColorHelper.textColorRed)
-            currentColorRule.append(ColorHelper.textColorBlue)
+            updatedColorRule.append(ColorHelper.textColorBlue)
+            updatedColorRule.append(ColorHelper.textColorRed)
         }
         else
         {
-            currentColorRule.append(ColorHelper.textColorBlue)
-            currentColorRule.append(ColorHelper.textColorRed)
+            updatedColorRule.append(ColorHelper.textColorRed)
+            updatedColorRule.append(ColorHelper.textColorBlue)
         }
+        self.currentColorRule = updatedColorRule
         self.isSetRuleDefault = !self.isSetRuleDefault
     }
     
