156d12449df612a3935b6eab24f717fa5ea66e17
John DongHyun Kim <zapping0320@gmail.com>
2019-09-27 06:22:59 +0900

    add project for Clock App
    set swift 4.2 version at build info

---
+++ Clock/Clock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clock
+//
+//  Created by 김동현 on 27/09/2019.
+//  Copyright © 2019 John Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
